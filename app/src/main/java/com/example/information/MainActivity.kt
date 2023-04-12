fcb5c98b3687b31b2e273d74bb46f73a91f31e0b
seong <015jun@naver.com>
2023-04-12 23:15:21 +0900

    first commit 2차 commit

--- app/src/main/java/com/example/information/MainActivity.kt
+++ app/src/main/java/com/example/information/MainActivity.kt
@@ -1,11 +1,29 @@
-package com.example.information
+package com.example.information;
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+public class MainActivity extends AppCompatActivity {
+     static final int REQUEST_INFO = 1;
+     TextView tVResult, tVResultLabel;
+     String str;
+
+     protected void onCreate(Bundle savedInstanceState) {
+          super.onCreate(savedInstanceState);
+          setContentView(R.layout.activity_main);
+          Button btnReq = (Button) findViewById(R.id.btnRequest);
+          Button btnEnd = (Button) findViewById(R.id.btnEnd);
+          tVResultLabel = (TextView) findViewById(R.id.tVResultLabel);
+          tVResult = (TextView) findViewById(R.id.tVResult);
+          EditText editTextId = findViewById(R.id.editTextID);
+     }
+
+     public void onClick(View view) {
+
+     }
 }
+
